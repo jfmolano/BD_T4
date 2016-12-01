216758ad48ad58d1f3b37c8ee2ccee4a7db4011a
jfmolano1587 <jose.francisco.molano@gmail.com>
2016-11-30 19:11:16 -0500

    Mongo insert function

--- loader/loader.py
+++ loader/loader.py
@@ -1,17 +1,38 @@
-with open("/home/pi/biographies.list") as f:
+from dateutil.parser import parse
+from pymongo import MongoClient
+
+with open('conf.json', 'r') as f:
+    try:
+        conf = json.load(f)
+    except ValueError:
+        conf = {}
+
+ip_mongo = conf["ip_mongo"]
+
+client = MongoClient(ip_mongo, 27017)
+db = client['Grupo03']
+collection_actores = db['actores_taller4']
+
+#with open("/home/pi/biographies.list") as f:
+with open("out.list") as f:
     rn = None
     db = None
     dd = None
     for line in f:
 	if line.startswith( '------' ):
-		if rn != None and db != None and dd != None:
-			print {"rn":rn,"db":db,"dd":dd}
+		try:
+			if rn != None and db != None:
+				if dd != None:
+					dd = parse(dd)
+				collection_actores.insert({"rn":rn,"db":parse(db),"dd":dd})
+		except:
+			print "Date format error"
 		rn = None
 		db = None
 		dd = None
 	if line.startswith( 'RN:' ):
-		rn = line.replace("RN: ","")
+		rn = line.replace("RN: ","").replace("\n","")
 	if line.startswith( 'DB:' ):
-		db = line.replace("DB: ","")
+		db = line.replace("DB: ","").replace("\n","").split(",")[0]
 	if line.startswith( 'DD:' ):
-		dd = line.replace("DD: ","")
+		dd = line.replace("DD: ","").replace("\n","").split(",")[0]
