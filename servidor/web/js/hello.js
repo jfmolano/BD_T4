d41e1bcb01142a0ed6052f70e4713c44a8e1fe6a
jfmolano1587 <jose.francisco.molano@gmail.com>
2016-12-02 14:29:06 -0500

    Questions and entities view closes #1

--- servidor/web/js/hello.js
+++ servidor/web/js/hello.js
@@ -11,7 +11,42 @@ $(document).ready(function() {
             var data_json = JSON.parse(data)
             console.log("data: ")
             console.log(data_json)
+            $.each(data_json, function (i, item) {
+                console.log(item)
 
+                var e_list = "<ul>"
+                $.each(item['entities'], function (j, jtem) {
+                    var value = ""
+                    if('geometry' in jtem){
+                        value = jtem["id"] + " - lugar/organización"
+                    }
+                    else if(jtem["type"] == 1){
+                        value = jtem["id"] + " - personaje"
+                    }
+                    else{
+                        value = jtem["id"] + " - otro"
+                    }
+                    e_list = e_list+"<li>"+value+"</li>"
+                });
+                e_list = e_list + "</ul>"
+
+                var tags_list = "<ul>"
+                tag_1 = item['tag_1']
+                tag_2 = item['tag_2']
+                tag_3 = item['tag_3']
+                if (tag_1 != null){
+                    tags_list = tags_list+"<li>"+tag_1+"</li>"
+                }
+                if (tag_2 != null){
+                    tags_list = tags_list+"<li>"+tag_2+"</li>"
+                }
+                if (tag_3 != null){
+                    tags_list = tags_list+"<li>"+tag_3["id"]+"</li>"
+                }
+                tags_list = tags_list + "</ul>"
+
+                $('#tabla_info_preguntas').append("<tr><td>"+item['question']+"</td><td>"+item['answer_1']+"</td><td>"+tags_list+"</td><td>"+e_list+"</td></tr>");
+            });
             /*$.each(lista, function (i, item) {
             $('#selector_consulta_1').append($('<option>', { 
                 value: item,
